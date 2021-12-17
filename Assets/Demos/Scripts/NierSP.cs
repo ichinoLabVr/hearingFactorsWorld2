4e928b9982413a3073e623d52d2e76a791519ddc
muraki111 <eitarou.0201@gmail.com>
2021-12-18 00:52:40 +0900

    update log

--- Assets/Demos/Scripts/NierSP.cs
+++ Assets/Demos/Scripts/NierSP.cs
@@ -55,7 +55,7 @@ public class NierSP : MonoBehaviourPunCallbacks
     void Nierlog()
     {
         float pos = Mathf.Sqrt(Mathf.Pow(tarpos.x - unipos.x, 2f) + Mathf.Pow(tarpos.z - unipos.z, 2f));
-        FileLog.AppendLog("log/NierSP.txt", System.DateTime.Now.ToString() + nierobj.name + " UserID=" + photonView.OwnerActorNr + " : " + pos + "unitychan : " + unipos + "\n");
+        FileLog.AppendLog("log/NierSP.txt", System.DateTime.Now.ToString() + " : " + nierobj.name + " UserID=" + photonView.OwnerActorNr + " : " + pos + "unitychan : " + unipos + "\n");
         if (!photonView.IsMine)
         {
             Debug.Log(tarpos + "" + unipos);
